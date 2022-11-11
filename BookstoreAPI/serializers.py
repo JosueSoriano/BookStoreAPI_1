d50e6da8aa8629a3e9d18479e2209cc5080a4a4c
Josué Soriano <josue.soriano@gmail.com>
2022-11-11 17:13:25 +0100

    Commit del proyecto de BookStore desarrollado en local con PyCharm

---
+++ BookstoreAPI/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Author, Book
+
+
+class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Author
+        fields = ('name', 'added_by_id', 'created_date')
+
+
+class BookSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('title', 'added_by_id', 'description', 'author', 'created_date')
+
